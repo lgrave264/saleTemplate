52f77013b576241b9a454ab5673f39af75f46d0a
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2022-03-21 14:37:02 -0700

    got css to work

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 // use res.render to load up an ejs view file
 
